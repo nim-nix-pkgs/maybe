4140e518fc5138493f7857f528a4554a2aaf08f8
hugosenari <hugosenari@gmail.com>
2022-03-12 03:19:29 -0300

    chore: re index maybe

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  description = ''basic monadic maybe type for Nim'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs."maybe-master".url = "path:./master";
+  inputs."maybe-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."maybe-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."maybe-1_0".url = "path:./1_0";
+  inputs."maybe-1_0".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."maybe-1_0".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."maybe-2_0".url = "path:./2_0";
+  inputs."maybe-2_0".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."maybe-2_0".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."maybe-2_2".url = "path:./2_2";
+  inputs."maybe-2_2".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."maybe-2_2".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib"];
+  in lib.mkProjectOutput {
+    inherit self nixpkgs;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+    refs = builtins.removeAttrs inputs args;
+  };
+}
---
+++ master/flake.nix
@@ -0,0 +1,26 @@
+{
+  description = ''basic monadic maybe type for Nim'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-maybe-master.flake = false;
+  inputs.src-maybe-master.owner = "superfunc";
+  inputs.src-maybe-master.ref   = "refs/heads/master";
+  inputs.src-maybe-master.repo  = "maybe";
+  inputs.src-maybe-master.type  = "github";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-maybe-master"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-maybe-master";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
