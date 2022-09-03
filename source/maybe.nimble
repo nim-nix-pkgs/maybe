fa32e885997762f4af8288470b509db40628d0cc
hugosenari <hugosenari@gmail.com>
2022-09-03 19:14:58 -0300

    chore: re index maybe

---
+++ source/maybe.nimble
@@ -0,0 +1,12 @@
+# Package
+
+version = "2.2"
+author = "superfunc"
+description = "A maybe type for nim."
+license = "BSD"
+srcDir = "src"
+skipDirs = @["examples", "tests"]
+
+# Deps
+
+requires "nim >= 0.9.6"
---
+++ source/src/tests/basic.nim
@@ -0,0 +1,60 @@
+# Copyright (c) 2017, Josh Filstrup
+# Licensed under BSD3(see license.md file for details)
+#
+
+import ../maybe/maybe
+
+proc add5(x : int) : int =
+  return x + 5
+
+proc testBasic() =
+  var 
+    a = maybe.just(5)
+    b = maybe.just(3)
+    c = maybe.nothing[int]()
+    
+  a = maybe.fmap(a, add5) 
+  b = maybe.fmap(b, add5)
+  c = maybe.fmap(c, add5)
+
+  maybeCase a:
+    just aInner:
+      assert aInner == 10, "Value in 'a' should be 10"
+      assert a.valid, "'a' should only be valid in this clause"
+      a = maybe.just(15)
+
+    nothing:
+      assert false, "This clause should be unreachable" 
+
+  maybeCase b:
+    just bInner:
+      assert bInner == 8, "Value in 'b' should be 8" 
+      assert b.valid, "'b' shoud only be valid in this clause"
+    nothing:
+      assert false, "This clause should be unreachable" 
+
+  maybeCase c:
+    just cInner:
+      assert false, "This clause should be unreachable" 
+    nothing:
+      assert c.valid == false, "'c' should only be invalid in this clause"
+  
+  assert $a == "Just 15"
+
+type Temp = enum
+  VHot, Hot, Med, Cold, VCold
+
+proc testCustomTypes() =
+  var 
+    a = maybe.just(VHot)
+    b = maybe.just(Cold)
+    c = maybe.nothing[Temp]()
+    d = maybe.just(VHot)
+  
+  # Using maybes eq operator
+  assert a != b, "The inner values should not be equal"
+  assert a == d, "These inner values should be equal"
+  assert c != b, "The inner values should not be equal"
+
+testBasic()
+testCustomTypes()
